6514ccb5967dd2e2d1ae2ca7755281c50ec49eb2
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-09-22 04:19:44 +0530

    Create finding element.c

---
+++ LinkedList/finding element.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for a node in the singly linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a new node at a given position
+void insertNodeAtPosition(struct Node** head, int data, int position) {
+    struct Node* newNode = createNode(data);
+    if (position < 1) {
+        printf("Invalid position.\n");
+        free(newNode);
+        return;
+    }
+    
+    if (position == 1) {
+        // Insert at the beginning
+        newNode->next = *head;
+        *head = newNode;
+    } else {
+        // Traverse to the node just before the position
+        struct Node* prevNode = *head;
+        for (int i = 1; i < position - 1 && prevNode != NULL; i++) {
+            prevNode = prevNode->next;
+        }
+        
+        if (prevNode == NULL) {
+            printf("Invalid position.\n");
+            free(newNode);
+            return;
+        }
+        
+        // Insert the new node after prevNode
+        newNode->next = prevNode->next;
+        prevNode->next = newNode;
+    }
+}
+
+// Function to find an element at a specific position
+int findElementAtPosition(struct Node* head, int position) {
+    if (position < 1) {
+        printf("Invalid position.\n");
+        return -1; // Return a sentinel value to indicate an error
+    }
+    
+    struct Node* current = head;
+    int currentPosition = 1;
+    
+    while (current != NULL && currentPosition != position) {
+        current = current->next;
+        currentPosition++;
+    }
+    
+    if (current == NULL) {
+        printf("Position not found.\n");
+        return -1; // Return a sentinel value to indicate an error
+    }
+    
+    return current->data;
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free memory allocated for the linked list
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Insert nodes at different positions
+    insertNodeAtPosition(&head, 1, 1); // 1 -> NULL
+    insertNodeAtPosition(&head, 2, 2); // 1 -> 2 -> NULL
+    insertNodeAtPosition(&head, 3, 2); // 1 -> 3 -> 2 -> NULL
+    insertNodeAtPosition(&head, 4, 4); // 1 -> 3 -> 2 -> 4 -> NULL
+
+    // Print the linked list
+    printf("Linked List: ");
+    printList(head);
+
+    // Find an element at a specific position
+    int positionToFind = 2;
+    int foundElement = findElementAtPosition(head, positionToFind);
+    if (foundElement != -1) {
+        printf("Element at position %d: %d\n", positionToFind, foundElement);
+    }
+
+    // Free memory allocated for the linked list
+    freeList(head);
+
+    return 0;
+}
